949dbe43f9ee39233deda3a2cb7b6409feeaf38a
Karen Sukiasyan <armenfilm@mail.ru>
2022-08-15 00:06:45 +0300

    Добавил 1

--- Seminar01/Program.cs
+++ Seminar01/Program.cs
@@ -53,3 +53,14 @@ else
     }
 }
 
+// Напишите программу , которая на вход принимает число и выдаёт его квадрат (число умноженное на само себя)
+
+Console.Clear();
+
+Console.Write("Enter number: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// int sqrt = number * number;
+// number *= number;
+int sqrt1 = Convert.ToInt32(Math.Pow(number, 2));
+
+Console.Write("number squared: " + sqrt1);
